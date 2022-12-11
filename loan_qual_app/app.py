2119ad1b0905cc999e3d8211ba5991e312ef10aa
Ivan Galeano <igaleano2@gmail.com>
2022-12-11 11:21:14 -0500

    Added folders for the challenge

--- loan_qual_app/app.py
+++ loan_qual_app/app.py
@@ -1,18 +1,28 @@
-from pathlib import Path
+# -*- coding: utf-8 -*-
+"""Loan Qualifier Application.
+
+This is a command line application to match applicants with qualifying loans.
+
+Example:
+    $ python app.py
+"""
+import sys
 import fire
 import questionary
+from pathlib import Path
 
-from qualifier.utils.fileio import load_csv, save_csv
+from qualifier.utils.fileio import load_csv
 
 from qualifier.utils.calculators import (
     calculate_monthly_debt_ratio,
-    calculate_loan_to_value_ratio
+    calculate_loan_to_value_ratio,
 )
 
 from qualifier.filters.max_loan_size import filter_max_loan_size
 from qualifier.filters.credit_score import filter_credit_score
 from qualifier.filters.debt_to_income import filter_debt_to_income
 from qualifier.filters.loan_to_value import filter_loan_to_value
+
 
 def load_bank_data():
     """Ask for the file path to the latest banking data and load the CSV file.
@@ -23,6 +33,8 @@ def load_bank_data():
 
     csvpath = questionary.text("Enter a file path to a rate-sheet (.csv):").ask()
     csvpath = Path(csvpath)
+    if not csvpath.exists():
+        sys.exit(f"Oops! Can't find this path: {csvpath}")
 
     return load_csv(csvpath)
 
@@ -89,6 +101,17 @@ def find_qualifying_loans(bank_data, credit_score, debt, income, loan, home_valu
 
     return bank_data_filtered
 
+
+def save_qualifying_loans(qualifying_loans):
+    """Saves the qualifying loans to a CSV file.
+
+    Args:
+        qualifying_loans (list of lists): The qualifying bank loans.
+    """
+    # @TODO: Complete the usability dialog for savings the CSV Files.
+    # YOUR CODE HERE!
+
+
 def run():
     """The main function for running the script."""
 
@@ -102,6 +125,10 @@ def run():
     qualifying_loans = find_qualifying_loans(
         bank_data, credit_score, debt, income, loan_amount, home_value
     )
+
+    # Save qualifying loans
+    save_qualifying_loans(qualifying_loans)
+
 
 if __name__ == "__main__":
     fire.Fire(run)
